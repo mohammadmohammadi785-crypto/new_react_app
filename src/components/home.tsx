96d10efe2175b6558c18e93031ca7e2bb6e144cf
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-06-18 14:48:49 +0430

    Changes added

---
+++ src/components/home.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <input
+        className="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        type="text"
+      />
+    </div>
+  );
+}
