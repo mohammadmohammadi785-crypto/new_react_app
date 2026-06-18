93248602ff6405092688643f3a13792f1d63788e
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-06-18 14:58:56 +0430

    Changes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Home from "./components/home";
+// import Home from "./components/home";
 
 function App() {
   const [count, setCount] = useState(0);
