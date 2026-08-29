95debb49f960756ef4c64d5e4a54584a2c48c7ea
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-08-29 19:46:06 +0430

    Changes added

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,10 +1,7 @@
 export default function Home() {
   return (
     <div className="flex shadow rounded-md flex-col items-center justify-center h-fit gap-4 p-4">
-      <input
-        className="border border-black rounded-md py-2 px-4 focus:outline-none"
-        type="text"
-      />
+      <h1>This is the home Page</h1>
     </div>
   );
 }
