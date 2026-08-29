4db43fc528720f4b616f7eacc501161a8d383164
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-08-29 23:23:48 +0430

    Changes added

--- src/components/home.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex shadow rounded-md flex-col items-center justify-center h-fit gap-4 p-4">
-      <h1>This is the home Page</h1>
-    </div>
-  );
-}
