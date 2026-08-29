634fe005a1244077bbbb0db142ea710d184899c2
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-08-29 19:45:08 +0430

    Changes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import { useState } from "react";
 import "./App.css";
 // import Home from "./components/home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="app">
-      <h1>React App</h1>
-      <p>سلام! این یک پروژهٔ React با Vite و TypeScript است.</p>
-      <button type="button" onClick={() => setCount((value) => value + 1)}>
-        شمارش: {count}
-      </button>
-      {/* <Home /> */}
+      <h1>THis is the main Page</h1>
     </div>
   );
 }
